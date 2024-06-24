e226335006f6ef7bdcb74381d0923b3fb9d8bfab
Rick Lan <ricklan@gmail.com>
2024-06-24 11:56:11 +0800

    road edge detection

--- selfdrive/controls/lib/road_edge_detector.py
+++ selfdrive/controls/lib/road_edge_detector.py
@@ -22,15 +22,19 @@
 # THE SOFTWARE.
 
 import numpy as np
+from openpilot.common.params import Params
 
 class RoadEdgeDetector:
 
   def __init__(self):
-    self._is_enabled = False
+    self._is_enabled = Params().get_bool("dp_lat_road_edge_detection")
 
-  def get_road_edge_detected(self, road_edge_stds, lane_line_probs, left_blinker, right_blinker):
+  def get_road_edge_detected(self, blindspot_detected, road_edge_stds, lane_line_probs, left_blinker, right_blinker):
+    if blindspot_detected:
+      return True
+
     if not self._is_enabled:
-      return False, False
+      return blindspot_detected
 
     # Calculate probabiities for detecting road edges and lane lines
     left_road_edge_prob = np.clip(1.0 - road_edge_stds[0], 0.0, 1.0)
@@ -41,14 +45,8 @@ class RoadEdgeDetector:
 
     # Check conditions for detecting road edges
     if right_blinker and right_road_edge_prob > 0.35 and right_lane_nearside_prob < 0.2 and left_lane_nearside_prob >= right_lane_nearside_prob:
-      return False, True
-    if left_blinker and left_road_edge_prob > 0.35 and left_lane_nearside_prob < 0.2 and right_lane_nearside_prob >= left_lane_nearside_prob:
-      return True, False
+      return True
+    elif left_blinker and left_road_edge_prob > 0.35 and left_lane_nearside_prob < 0.2 and right_lane_nearside_prob >= left_lane_nearside_prob:
+      return True
 
-    return False, False
-
-  def set_enabled(self, enabled):
-    self._is_enabled = enabled
-
-  def is_enabled(self):
-    return self._is_enabled
+    return blindspot_detected
