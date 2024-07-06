af173ae2139ccec7f3b9bc2034d4059bf8e2bbd2
Rick Lan <ricklan@gmail.com>
2024-07-06 22:55:50 +0800

    missed changes

--- selfdrive/tetood/lib/overpass_api_helper.py
+++ selfdrive/tetood/lib/overpass_api_helper.py
@@ -4,43 +4,33 @@ import numpy as np
 
 class OverpassAPIHelper:
     def __init__(self):
-        # self.url = 'https://overpass-api.de/api/interpreter'
-        self.url = 'https://overpass.kumi.systems/api/interpreter'
+        self.url = 'https://overpass-api.de/api/interpreter'
+        # self.url = 'https://overpass.kumi.systems/api/interpreter'
         self.headers = {'Accept-Encoding': 'gzip'}
 
-    def fetch_data(self, lat, lon, radius=1300):
+    def fetch_data(self, lat, lon, radius=3500, high_speed=True):
         R = 6373000.0  # approximate radius of earth in mts
         """Fetch data from Overpass API based on the given GPS coordinates."""
-        # overpass_query = f"""
-        # [out:json];
-        # (
-        #   way(around:{radius},{lat},{lon})
-        #   [highway][highway!~"^(pedestrian|footway|path|bridleway|steps|cycleway|construction|bus_guideway|escape)$"];
-        # );
-        # out body;
-        # """
-        # overpass_query = f"""
-        # [out:json];
-        # (
-        #   way(around:{radius},{lat},{lon})
-        #   [highway][highway!~"^(pedestrian|footway|path|bridleway|steps|cycleway|construction|bus_guideway|escape)$"];
-        #   >;
-        # );
-        # out body;
-        # """
-        # Calculate the bounding box coordinates for the bbox containing the circle around location.
         bbox_angle = np.degrees(radius / R)
         # fetch all ways and nodes on this ways in bbox
         bbox_str = f'{str(lat - bbox_angle)},{str(lon - bbox_angle)},{str(lat + bbox_angle)},{str(lon + bbox_angle)}'
-        # lat_lon = "(%f,%f)" % (lat, lon)
-        q = """
-            [out:json];
-            way(""" + bbox_str + """)
-              [highway]
-              [highway!~"^(footway|path|corridor|bridleway|steps|cycleway|construction|bus_guideway|escape|service|track)$"];
-            (._;>;);
-            out;"""
-        overpass_query = q
-        print(overpass_query)
+        excl_way_types = 'pedestrian|footway|path|corridor|bridleway|steps|cycleway|construction|bus_guideway|escape|track'
+        overpass_query = f"""
+        [out:json][timeout:25][bbox:{bbox_str}];
+        (
+          way[highway][highway!~"^({excl_way_types})$"];
+        ) -> .allways;
+        (
+          way[highway][highway!~"^({excl_way_types})$"][!name];
+        ) -> .no_name_ways;
+        (
+          way[highway][highway!~"^({excl_way_types})$"][service][service~"^(alley|driveway)$"];
+        ) -> .service_ways;
+        (.allways; - .no_name_ways;) -> .way_result_1;
+        (.way_result_1; - .service_ways;) -> .way_result_final;
+        (.way_result_final;>;);
+        out body;
+        """
+        # print(overpass_query)
         response = requests.get(self.url, params={'data': overpass_query}, headers=self.headers)
         return response.json()
