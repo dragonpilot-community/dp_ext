51f988686e1a4785eda05c91a6409b6d9aa1faa9
Rick Lan <ricklan@gmail.com>
2024-07-19 11:28:36 +0800

    clean up

--- selfdrive/controls/lib/personalized_accel_learner.py
+++ selfdrive/controls/lib/personalized_accel_learner.py
@@ -9,6 +9,11 @@ from openpilot.selfdrive.car.interfaces import ACCEL_MAX
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 
+# The 0.5 m/s intervals up to 2 m/s (roughly 0-7.2 km/h or 0-4.5 mph) allow for very precise control during start-stop traffic.
+# The range from 2 m/s to 15 m/s (7.2-54 km/h or 4.5-33.6 mph) covers most urban driving scenarios.
+# The range from 20 m/s to 40 m/s (72-144 km/h or 44.7-89.5 mph) covers highway driving speeds.
+A_MAX_BP = [0, 0.5, 1, 1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 32.5, 35, 37.5, 40]
+SAMPLES = [10, 10, 10, 10, 10, 10, 15, 15, 15, 15, 20, 20, 30, 30, 30, 30, 30, 30, 30]
 
 # other constants
 SMOOTH_FACTOR = 0.9
@@ -24,15 +29,9 @@ class PersonalizedAccelLearner:
         self.dp_long_pal_launch_boost = 0. # percentage
         self.frame = 0
 
-        self.samples_per_range = self._initialize_variable_samples()
+        self.profile_size = len(A_MAX_BP)
 
-        # The 0.5 m/s intervals up to 2 m/s (roughly 0-7.2 km/h or 0-4.5 mph) allow for very precise control during start-stop traffic.
-        # The range from 2 m/s to 15 m/s (7.2-54 km/h or 4.5-33.6 mph) covers most urban driving scenarios.
-        # The range from 20 m/s to 40 m/s (72-144 km/h or 44.7-89.5 mph) covers highway driving speeds.
-        self.A_MAX_BP = [0, 0.5, 1, 1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 25, 30, 32.5, 35, 37.5, 40]
-        self.profile_size = len(self.A_MAX_BP)
-
-        self.MAX_VALS = np.maximum(0, np.interp(self.A_MAX_BP, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)).tolist()
+        self.MAX_VALS = np.maximum(0, np.interp(A_MAX_BP, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)).tolist()
         self.A_CRUISE_MAX_VALS = self.MAX_VALS.copy()
 
         self.speed_range_samples = {i: deque(maxlen=MAX_SAMPLES_PER_RANGE) for i in range(self.profile_size)}
@@ -42,7 +41,7 @@ class PersonalizedAccelLearner:
         self.load_model()
 
     def _initialize_variable_samples(self):
-        return [10, 10, 10, 10, 10, 10, 15, 15, 15, 15, 20, 20, 25, 30, 35, 35, 40, 40, 40]
+        return [10, 10, 10, 10, 10, 10, 15, 15, 15, 15, 20, 20, 20, 20, 25, 25, 25, 25, 25]
 
     def update(self, car_state, lead_present):
         if (not lead_present and
@@ -54,8 +53,8 @@ class PersonalizedAccelLearner:
             speed_index = self._get_speed_index(car_state.vEgo)
             self.speed_range_samples[speed_index].append(car_state.aEgo)
 
-            if speed_index < len(self.samples_per_range):
-                if len(self.speed_range_samples[speed_index]) >= self.samples_per_range[speed_index]:
+            if speed_index < len(SAMPLES):
+                if len(self.speed_range_samples[speed_index]) >= SAMPLES[speed_index]:
                     self._update_cruise_profile_for_index(speed_index)
 
             self.frame += 1
@@ -65,7 +64,7 @@ class PersonalizedAccelLearner:
         if self.frame % 60 == 0:
             try:
                 if self._params.get_bool("dp_long_pal_reset"):
-                    self.MAX_VALS = np.maximum(0, np.interp(self.A_MAX_BP, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)).tolist()
+                    self.MAX_VALS = np.maximum(0, np.interp(A_MAX_BP, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)).tolist()
                     self.A_CRUISE_MAX_VALS = self.MAX_VALS.copy()
                     self._params.remove("dp_long_pal_reset")
                     self.save_model()
@@ -81,8 +80,8 @@ class PersonalizedAccelLearner:
 
 
     def _get_speed_index(self, speed):
-        index = bisect.bisect_right(self.A_MAX_BP, speed) - 1
-        return max(0, min(index, len(self.A_MAX_BP) - 1))
+        index = bisect.bisect_right(A_MAX_BP, speed) - 1
+        return max(0, min(index, len(A_MAX_BP) - 1))
 
     def _update_cruise_profile_for_index(self, index):
         samples = list(self.speed_range_samples[index])
@@ -96,7 +95,7 @@ class PersonalizedAccelLearner:
 
     def get_max_accel(self, v_ego, STOCK_BP, STOCK_VALS):
         if self.enabled:
-            val = np.interp(v_ego, self.A_MAX_BP, self.A_CRUISE_MAX_VALS)
+            val = np.interp(v_ego, A_MAX_BP, self.A_CRUISE_MAX_VALS)
         else:
             val = np.interp(v_ego, STOCK_BP, STOCK_VALS)
         if self.dp_long_pal_launch_boost > 0. and v_ego <= 5.:
@@ -109,7 +108,6 @@ class PersonalizedAccelLearner:
 
     def save_model(self):
         data = {
-            'bp': self.A_MAX_BP,
             'vals': self._numpy_to_list(self.A_CRUISE_MAX_VALS)
         }
         self._params.put("dp_long_pal_vals", json.dumps(data))
@@ -121,7 +119,7 @@ class PersonalizedAccelLearner:
           params_val = None
         if params_val is not None:
             data = json.loads(params_val)
-            self.A_CRUISE_MAX_VALS = np.maximum(0, np.interp(self.A_MAX_BP, data.get('bp'), data.get('vals'))).tolist()
+            self.A_CRUISE_MAX_VALS = data.get('vals', self.A_CRUISE_MAX_VALS)
 
     def _numpy_to_list(self, obj):
         if isinstance(obj, np.ndarray):
